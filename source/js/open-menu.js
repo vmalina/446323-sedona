ceea003ff546aaf3b58fac6a2cc9ab3defb27f22
vmalina <Pollysonik@yandex.ru>
2018-05-06 14:44:04 +0400

    верстка сетки и декоративных элементов

--- source/js/open-menu.js
+++ source/js/open-menu.js
@@ -1,23 +1,26 @@
 'use strict';
 (function (){
-  var pageHeader = document.querySelector('.page-header')
-  var toggleOpen = pageHeader.querySelector('.page-header__togle--open');
-  var toggleClose = pageHeader.querySelector('.page-header__togle--close');
-  var menu = pageHeader.querySelector('.main-menu');
+  var menu = document.querySelector('.menu')
+  var menuList = menu.querySelector('.menu__list');
+  var buttonClose = menu.querySelector('.menu__button--close');
+  var buttonOpen = menu.querySelector('.menu__button--open');
 
-  menu.classList.remove('main-menu__menu‐‐open');
-  toggleOpen.addEventListener('click', openMenu);
+  menuList.classList.remove('menu__list--open');
+  buttonOpen.classList.add('menu__button--show');
+
+  buttonOpen.addEventListener('click', openMenu);
 
   function openMenu() {
     event.preventDefault();
-    pageHeader.classList.add('page-header__open-menu');
-    menu.classList.add('main-menu__menu‐‐open');
-    toggleClose.addEventListener('click', closeMenu);
+    menuList.classList.add('menu__list--open');
+    buttonClose.addEventListener('click', closeMenu);
+    buttonOpen.classList.remove('menu__button--show');
   }
   function closeMenu() {
     event.preventDefault();
-    pageHeader.classList.remove('page-header__open-menu');
-    menu.classList.remove('main-menu__menu‐‐open');
-    toggleClose.removeEventListener('click', closeMenu);
+    menuList.classList.remove('menu__list--open');
+    buttonOpen.classList.add('menu__button--show');
+    buttonClose.removeEventListener('click', closeMenu);
+    buttonOpen.focus();
   }
 })();
