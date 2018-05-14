3aa8ccb84a2afd6c47a353bc17f0cf5a394922fc
vmalina <Pollysonik@yandex.ru>
2018-05-15 01:19:17 +0400

    модальное окно0

--- source/js/open-menu.js
+++ source/js/open-menu.js
@@ -1,6 +1,6 @@
 'use strict';
 (function (){
-  var menu = document.querySelector('.menu')
+  var menu = document.querySelector('.menu');
   var menuList = menu.querySelector('.menu__list');
   var buttonClose = menu.querySelector('.menu__button--close');
   var buttonOpen = menu.querySelector('.menu__button--open');
@@ -10,14 +10,14 @@
 
   buttonOpen.addEventListener('click', openMenu);
 
-  function openMenu() {
-    event.preventDefault();
+  function openMenu(evt) {
+    evt.preventDefault();
     menuList.classList.add('menu__list--open');
     buttonClose.addEventListener('click', closeMenu);
     buttonOpen.classList.remove('menu__button--show');
   }
-  function closeMenu() {
-    event.preventDefault();
+  function closeMenu(evt) {
+    evt.preventDefault();
     menuList.classList.remove('menu__list--open');
     buttonOpen.classList.add('menu__button--show');
     buttonClose.removeEventListener('click', closeMenu);
