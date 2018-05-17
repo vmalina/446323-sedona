177af6c90db504513f8a42e24dab642bca46db9d
vmalina <Pollysonik@yandex.ru>
2018-05-18 03:38:33 +0400

    Исправление замечаний

--- source/js/open-menu.js
+++ source/js/open-menu.js
@@ -6,7 +6,6 @@
   var buttonOpen = menu.querySelector('.menu__button--open');
 
   menuList.classList.remove('menu__list--open');
-  buttonOpen.classList.add('menu__button--show');
 
   buttonOpen.addEventListener('click', openMenu);
 
@@ -14,12 +13,11 @@
     event.preventDefault();
     menuList.classList.add('menu__list--open');
     buttonClose.addEventListener('click', closeMenu);
-    buttonOpen.classList.remove('menu__button--show');
   }
+
   function closeMenu(event) {
     event.preventDefault();
     menuList.classList.remove('menu__list--open');
-    buttonOpen.classList.add('menu__button--show');
     buttonClose.removeEventListener('click', closeMenu);
     buttonOpen.focus();
   }
